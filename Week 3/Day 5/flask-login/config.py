259a66d31416e3776d044bc5c152bb3963f163d1
Atpdevil <gokulteck@gmail.com>
2025-10-13 19:51:34 +0500

    Deleted day 5

--- Week 3/Day 5/flask-login/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from datetime import timedelta
-
-class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwt-dev-secret")
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=60)   # adjust lifetime
-    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=7)
--- Week 3/Day 5/flask-login/tests/test_routes.py
+++
@@ -1,6 +0,0 @@
-# tests/test_routes.py
-def test_public_route(client):
-    rv = client.get("/api/public")
-    assert rv.status_code == 200
-    data = rv.get_json()
-    assert data["msg"] == "this is public"
